5a040b429e0d8635f4f23eda4b64f5acc4435434
Himmad Ameen <93907951+himadameen@users.noreply.github.com>
2022-06-06 17:44:22 +0530

    my changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,42 @@
-import React from "react";
-import Text from "./Text";
+import React, { Component, useState } from "react";
+import '../styles/App.css';
 
-const App = () => {
-  return (
-    <>
-      <div id="main">
-        <Text
-          name={
-            "Now I can render any React component on any DOM node I want using ReactDOM.render"
-          }
-        />
-      </div>
-    </>
-  );
-};
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            renderBall: false,
+            posi : 0,
+            ballPosition: { left: "0px" }
+        };
+        this.renderChoice = this.renderBallOrButton.bind(this)
+        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+    };
+
+    buttonClickHandler() {
+   
+   }
+    renderBallOrButton() {
+		if (this.state.renderBall) {
+		    return <div className="ball" style={this.state.ballPosition}></div>
+		} else {
+		    return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
+		}
+    }
+
+    // bind ArrowRight keydown event
+    componentDidMount() {
+      
+    }
+
+    render() {
+        return (
+            <div className="playground">
+                {this.renderBallOrButton()}
+            </div>
+        )
+    }
+}
+
 
 export default App;
